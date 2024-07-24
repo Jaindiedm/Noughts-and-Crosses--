778858b54343032541ff1790f132fc94c3914681
Jaindi Hirunima <jhwalisundara@gmail.com>
2024-07-24 20:20:19 +0000

    add comments to validat.py

--- validat.py
+++ validat.py
@@ -1,11 +1,16 @@
-def validater(num,endpoint):
+def validater(num, endpoint):
+    # Check if the input 'num' is a digit
     if num.isdigit():
+        # Convert 'num' to an integer
         num = int(num)
+        # Check if 'num' is within the valid range
         if num < endpoint and num > 0:
-            return num,True
+            return num, True
         else:
+            # Print an error message if 'num' is out of range
             print(f"Enter number between 1 and {endpoint-1}.")
-            return num,False
+            return num, False
     else:
+        # Print an error message if 'num' is not a valid digit
         print("Invalid input")
-        return num,False
+        return num, False
